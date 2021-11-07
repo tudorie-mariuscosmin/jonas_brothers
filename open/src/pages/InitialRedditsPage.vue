ca52dceee3f4ecc314389c6096c3dd30f0166963
Marius Tudorie <tudorie.mariuscosmin@gmail.com>
2021-11-07 02:37:47 +0200

    dialog

--- open/src/pages/InitialRedditsPage.vue
+++ open/src/pages/InitialRedditsPage.vue
@@ -23,6 +23,41 @@
       label="Please wait..."
       label-style="font-size: 1.1em"
     />
+    <q-dialog seamless v-model="dialog" position="bottom">
+      <q-card style="width: 350px">
+        <q-btn flat round icon="close" class="float-right" v-close-popup />
+        <q-card-section class="row justify-between items-center no-wrap">
+          <div class="column">
+            <div>
+              <q-avatar size="70px" class="shadow-1">
+                <q-img
+                  fit="contain"
+                  v-if="selected.photo"
+                  :src="selected.photo"
+                />
+                <template v-else>{{ selected.name[0] }}</template>
+              </q-avatar>
+            </div>
+          </div>
+          <div>
+            <div class="row no-wrap">
+              <div class="column justify-start" style="width: 150px">
+                <div class="text-h6">
+                  {{ selected.name }}
+                </div>
+                <q-btn
+                  :flat="selected.isFollowed"
+                  :outline="!selected.isFollowed"
+                  :color="selected.isFollowed ? '' : 'green'"
+                  @click="() => subscribe(selected)"
+                  >{{ selected.isFollowed ? "subscribed" : "subscribe" }}</q-btn
+                >
+              </div>
+            </div>
+          </div>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 <script>
@@ -36,6 +71,8 @@ export default {
     return {
       subs: [],
       isLoading: false,
+      dialog: false,
+      selected: null,
     };
   },
   async created() {
@@ -52,6 +89,13 @@ export default {
   },
   methods: {
     async handleClick(sub) {
+      this.selected = sub;
+      this.dialog = true;
+    },
+    closeDialog() {
+      this.dialog = false;
+    },
+    async subscribe(sub) {
       if (sub.isFollowed) {
         await this.$axios.delete(`/api/subreddits/${sub.name}`);
         sub.isFollowed = false;
